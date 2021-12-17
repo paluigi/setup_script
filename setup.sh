3b1011f252b5dbb2046fdd54a83e5386683d9c1f
Luigi <paluigi@gmail.com>
2021-12-17 15:15:13 +0000

    Script until python modules installation

--- setup.sh
+++ setup.sh
@@ -14,4 +14,10 @@ sudo apt install gnome-keyring
 git config --global user.name "Luigi"
 git config --global user.email paluigi@gmail.com
 
+# from now can push/pull repositories
+git config credential.helper 'cache --timeout=15000'
+
 python3 -m pip install --upgrade pip
+
+# Clone repo
+git clone https://github.com/paluigi/selenium1.git
