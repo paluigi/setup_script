01480f3edabc5f4b0b0740c6a93cbb0a87ae927c
Luigi <paluigi@gmail.com>
2021-12-17 15:07:42 +0000

    First commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt upgrade
+
+# Optional
+sudo apt install unzip xvfb libxi6 libgconf-2-4
+
+chromium --product-version
+sudo apt install chromium-driver
+
+sudo apt install python3-pip
+sudo apt install python3-dev python3-setuptools python3-wheel
+
+sudo apt install gnome-keyring
+git config --global user.name "Luigi"
+git config --global user.email paluigi@gmail.com
+
+python3 -m pip install --upgrade pip
